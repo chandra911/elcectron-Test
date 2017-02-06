698bd4aceafef920622379b7e1608c398808e9bc
Kumar <chandra.bhu.kumar@hpe.com>
2017-02-06 17:07:02 +0530

    f

--- main.js
+++ main.js
@@ -23,15 +23,15 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+ // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+ // mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+   // mainWindow = null
+  //})//
 }
 
 // This method will be called when Electron has finished
